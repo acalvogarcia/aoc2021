28850fc29fa3c3c77f07282586d61e483ced15a2
Álvaro Calvo García <cokeiv@gmail.com>
2021-12-05 20:13:43 +0100

    Day 5

---
+++ day_5/puzzle_1/main.hs
@@ -0,0 +1,28 @@
+import Data.List (group, sort)
+import Data.List.Split (splitOn)
+
+main :: IO ()
+main = do
+  userInput <- getContents
+  print $ getSafePointsAmount $ getTravelledPoints $ mapInput userInput
+
+mapInput :: String -> [[[Int]]]
+mapInput = map (map mapPoint . splitOn " -> ") . lines
+  where
+    mapPoint x = map read (splitOn "," x) :: [Int]
+
+getTravelledPointsFromMovement :: [[Int]] -> [[Int]]
+getTravelledPointsFromMovement [[a,b], [c,d]]
+  | a==c && b==d = [[a,b]]
+  | b==d && a<c = [a,b]:getTravelledPointsFromMovement [[a+1, b], [c,d]]
+  | b==d && a>c = [a,b]:getTravelledPointsFromMovement [[a-1, b], [c,d]]
+  | a==c && b<d = [a,b]:getTravelledPointsFromMovement [[a, b+1], [c,d]]
+  | a==c && b>d = [a,b]:getTravelledPointsFromMovement [[a, b-1], [c,d]]
+  | otherwise = []
+getTravelledPointsFromMovement _ = error "This shouldn't be here"
+
+getTravelledPoints :: [[[Int]]] -> [[[Int]]]
+getTravelledPoints = map getTravelledPointsFromMovement
+
+getSafePointsAmount :: [[[Int]]] -> Int
+getSafePointsAmount = length . map head . filter (\xs -> length xs > 1) . group . sort . concat
---
+++ day_5/puzzle_1/main.py
@@ -0,0 +1,52 @@
+from typing import List
+
+class Movement:
+    def __init__(self, line):
+        line_split = list(map(lambda s: s.split(","), line.split(" -> ")))
+        self.x1 = int(line_split[0][0])
+        self.y1 = int(line_split[0][1])
+        self.x2 = int(line_split[1][0])
+        self.y2 = int(line_split[1][1])
+
+    def get_points_travelled(self):
+        if self.x1 != self.x2 and self.y1 != self.y2:
+            return
+        if self.x1 != self.x2:
+            if self.x2 > self.x1:
+                for x in range(self.x1, self.x2+1):
+                    yield (x, self.y1)
+            else:
+                for x in range(self.x1, self.x2-1, -1):
+                    yield (x, self.y1)
+        if self.y1 != self.y2:
+            if self.y2 > self.y1:
+                for y in range(self.y1, self.y2+1):
+                    yield (self.x1, y)
+            else:
+                for y in range(self.y1, self.y2-1, -1):
+                    yield (self.x1, y)
+
+    def __repr__(self):
+        return f"({self.x1}, {self.y1}) -> ({self.x2}, {self.y2})"
+
+
+def read_input():
+    with open("input.txt", "r") as input_file:
+        lines = []
+        for line in input_file:
+            lines.append(Movement(line))
+        
+    return lines
+
+data: List[Movement] = read_input()
+
+all_points_travelled = {}
+
+for movement in data:
+    for point in movement.get_points_travelled():
+        if point in all_points_travelled:
+            all_points_travelled[point] += 1
+        else:
+            all_points_travelled[point] = 1
+
+print(len(list(filter(lambda x: x > 1, all_points_travelled.values()))))
