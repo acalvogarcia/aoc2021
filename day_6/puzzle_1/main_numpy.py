f5b745b4772af29f510bbbb6864816938552aa11
Álvaro Calvo García <cokeiv@gmail.com>
2021-12-07 00:22:39 +0100

    Day 6: Numpy matrices solution

---
+++ day_6/puzzle_1/main_numpy.py
@@ -0,0 +1,27 @@
+import numpy
+from numpy.linalg import matrix_power
+
+def read_input():
+    with open("input.txt", "r") as input_file:
+        return list(map(int, input_file.readline().split(",")))
+
+data = read_input()
+
+lanternfish_start = numpy.array([data.count(i) for i in range(9)])
+day_transformation = numpy.array([
+    [1 if j==(i+1)%9 or (j==0 and i==6) else 0 for j in range(9)]
+    for i in range(9)
+])
+lanternfish_end = matrix_power(day_transformation, 80).dot(lanternfish_start)
+print(sum(lanternfish_end))
+
+# The day transformation matrix is:
+# [0 1 0 0 0 0 0 0 0]
+# [0 0 1 0 0 0 0 0 0]
+# [0 0 0 1 0 0 0 0 0]
+# [0 0 0 0 1 0 0 0 0]
+# [0 0 0 0 0 1 0 0 0]
+# [0 0 0 0 0 0 1 0 0]
+# [1 0 0 0 0 0 0 1 0]
+# [0 0 0 0 0 0 0 0 1]
+# [1 0 0 0 0 0 0 0 0]
