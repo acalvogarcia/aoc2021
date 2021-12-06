4c963b47c35a67ac71a333d11c4fcfd8a15febfc
Álvaro Calvo García <cokeiv@gmail.com>
2021-12-06 23:16:44 +0100

    Day 6: Clean code

--- day_6/puzzle_1/main.hs
+++ day_6/puzzle_1/main.hs
@@ -11,8 +11,7 @@ mapInput xs = map read (splitOn "," xs) :: [Int]
 
 getInputMap :: (Ord a, Num a, Enum a) => [a] -> Map.Map a Int
 getInputMap xs = Map.fromList [ (day, count day xs) | day <- [0..8]]
-  where
-    count day xs = length $ filter (==day) xs
+  where count day xs = length $ filter (==day) xs
 
 getLanternfishAmount :: (Eq t, Num t, Ord k, Num k, Num p, Enum k) => t -> Map.Map k p -> p
 getLanternfishAmount 0 map = sum $ Map.elems map
@@ -20,4 +19,3 @@ getLanternfishAmount day map = getLanternfishAmount (day-1) newMap
   where
     newMap = Map.fromList $ (8, bornLanternfish):(6, bornLanternfish + Map.findWithDefault 0 7 map):[ (i, Map.findWithDefault 0 (i+1) map) | i <- 7:[0..5]]
     bornLanternfish = Map.findWithDefault 0 0 map
-    count day xs = length $ filter (==day) xs
